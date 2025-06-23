981499b55ffdc7701f5f9c218d28e56eee8f1479
abirami24052004 <abirami24052005@gmail.com>
2025-06-23 21:13:32 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { User, Plus, Edit3, Trash2, CheckCircle, Circle, LogOut, Mail ,Facebook, Github} from 'lucide-react';
 import { authAPI, tasksAPI } from './services/api';
-import { useEffect } from "react";
+
 import { useLocation, useNavigate } from "react-router-dom";
 
 
